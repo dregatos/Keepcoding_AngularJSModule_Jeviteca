53a30f73d503e06da253938857808184364eb424
David Regatos <david.regatos@gmail.com>
2015-06-15 11:59:31 +0200

    added .gitignore;

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 /**
  * Created by dregatos on 15/06/15.
  */
-<!-- Define our main module 'jeviteca' -->
+<!-- Define our main module 'jeviteca', i.e. our web app -->
 <!-- Inject necessary dependencies -->
+<!-- Every injected dependency is already instantiated -->
 angular.module("jeviteca", ["ngRoute", "route-segment", "view-segment"]);
