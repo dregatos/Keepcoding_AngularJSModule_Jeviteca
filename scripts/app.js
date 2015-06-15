aaa75d49a8bedad82c694dc21da80c96d3a23c4a
David Regatos <david.regatos@gmail.com>
2015-06-15 11:41:39 +0200

    defined & loaded the app's main module;

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,6 @@
 /**
  * Created by dregatos on 15/06/15.
  */
+<!-- Define our main module 'jeviteca' -->
+<!-- Inject necessary dependencies -->
+angular.module("jeviteca", ["ngRoute", "route-segment", "view-segment"]);
